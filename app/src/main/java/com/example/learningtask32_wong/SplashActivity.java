bf3805e99e9bbb40519a3ac01eafe7217934d304
annic <wongannica@gmail.com>
2022-10-28 19:13:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/learningtask32_wong/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.learningtask32_wong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MediaPlayer appIntro;
+        appIntro = MediaPlayer.create(SplashActivity.this,R.raw.app_intro);
+        appIntro.start();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                appIntro.stop();
+                finish();
+            }
+        }, 5000);
+    }
+}
